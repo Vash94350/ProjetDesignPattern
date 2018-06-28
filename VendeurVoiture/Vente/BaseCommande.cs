319da1ed26172caa99dac5e96dc32121ad8b2439
Vash94350 <michael.pean.mp@gmail.com>
2018-06-28 10:57:02 +0200

    ajout d'un module permettant de faire des commandes depuis divers pays (fonction de la TVA)

---
+++ VendeurVoiture/Vente/BaseCommande.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VendeurVoiture.Vente
+{
+    internal abstract class BaseCommande : ICommande
+    {
+        public double CalculeMontant()
+        {
+            return CalculeTVA() + CalculePreTaxMontant();
+        }
+
+        protected abstract double CalculeTVA();
+        protected abstract double CalculePreTaxMontant();
+    }
+}
+}
---
+++ VendeurVoiture/Vente/ICommande.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VendeurVoiture.Vente
+{
+    interface ICommande
+    {
+        double CalculaMontant();
+    }
+}
