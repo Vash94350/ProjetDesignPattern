a8c7450b77ed54461b903c993605f68ba8367437
Vash94350 <michael.pean.mp@gmail.com>
2018-06-29 12:34:39 +0200

    ajout de fichier a l'index + code clair dans la sortie console

--- VendeurVoiture/Program.cs
+++ VendeurVoiture/Program.cs
@@ -16,8 +16,11 @@ namespace VendeurVoiture
             boitagantparent.AddRangemment(boitagantbanal);
 
             //Module Fabrique
+            Console.WriteLine("Partie Fabrication : ");
             Fabrique.FabriqueAcier fabriqueAcier = new Fabrique.FabriqueAcier();
+            Fabrique.FabriqueCarbone fabriqueCarbone = new Fabrique.FabriqueCarbone();
             Fabrique.Voiture voiture = fabriqueAcier.CreateVoiture("voiture en acier", "v00001");
+            Fabrique.Voiture voiture2 = fabriqueCarbone.CreateVoiture("voiture en carbone", "v00002");
             voiture.AddRangement(boitagantparent);
             Console.WriteLine("Voiture cree : " + voiture.Name);
             Console.WriteLine("Ella a " + voiture.NombreDeRoues + " roues");
@@ -26,9 +29,11 @@ namespace VendeurVoiture
             {
                 Console.WriteLine("Le rangement : " + i.Name + " de taille : " + i.Size);
             }
+            Console.WriteLine();
 
 
             //Module Conception
+            Console.WriteLine("Partie Conception : ");
             Conception.ComposantFabric atelier = new Conception.ComposantFabric();
             Conception.Composant moteur20CV = atelier.getComposant("moteur20CV");
             moteur20CV.SetPrice(new Vente.Price(100.0, "EUR"));
@@ -38,16 +43,20 @@ namespace VendeurVoiture
             voiture.AddComposant(moteur20CV);
             voiture.AddComposant(tableauDeBordGPSIntegre);
             Console.WriteLine("Les composant de " + voiture.Name + " sont : " + voiture.getListDesComposants());
+            Console.WriteLine();
 
             //Module Vente
+            Console.WriteLine("Partie Vente : ");
             Vente.CommandeEnFrance commande_MonsieurDupont = new Vente.CommandeEnFrance(voiture);
             Console.WriteLine("Monsieur Dupont devra payer un montant total de : " + commande_MonsieurDupont.CalculeMontant() + " " + commande_MonsieurDupont.getDevise());
+            Console.WriteLine();
 
             //Module Comparateur avec en plus le pattern Singleton
+            Console.WriteLine("Partie Stock : ");
             Console.WriteLine("Voici le stock de voiture : ");
             foreach (Fabrique.Voiture voiturei in Stock.StockDeVoiture.Instance.GetVoitureDisponnible(DateTime.Now))
             {
-                Console.WriteLine("Voici la " + voiturei.Name + " avec pour reference : " + voiturei.Reference);
+                Console.WriteLine("- " + voiturei.Name + " avec pour reference : " + voiturei.Reference);
             }
             Console.WriteLine();
 
